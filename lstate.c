2270f3eb35b1dd2c6a470d36ed2d23b465a17b65
刘淩峰 <liulingfeng01@corp.netease.com>
2025-09-08 17:49:50 +0800

    添加了来自新版lua的closethread

--- lstate.c
+++ lstate.c
@@ -341,6 +341,14 @@ int luaE_resetthread (lua_State *L, int status) {
   return status;
 }
 
+LUA_API int lua_closethread (lua_State *L, lua_State *from) {
+  int status;
+  lua_lock(L);
+  L->nCcalls = (from) ? getCcalls(from) : 0;
+  status = luaE_resetthread(L, L->status);
+  lua_unlock(L);
+  return status;
+}
 
 LUA_API int lua_resetthread (lua_State *L) {
   int status;
